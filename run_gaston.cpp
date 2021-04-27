ff791ad9adbf2e9b2908d2752bd54691e97eb902
wenwenqu <iring.qu@gmail.com>
2021-04-27 21:42:10 +0800

    add gaston

---
+++ run_gaston.cpp
@@ -0,0 +1,88 @@
+//########################################################################
+//## Copyright 2019 Da Yan http://www.cs.uab.edu/yanda
+//##
+//## Licensed under the Apache License, Version 2.0 (the "License");
+//## you may not use this file except in compliance with the License.
+//## You may obtain a copy of the License at
+//##
+//## //http://www.apache.org/licenses/LICENSE-2.0
+//##
+//## Unless required by applicable law or agreed to in writing, software
+//## distributed under the License is distributed on an "AS IS" BASIS,
+//## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//## See the License for the specific language governing permissions and
+//## limitations under the License.
+//########################################################################
+
+//########################################################################
+//## Contributors
+//## * Wenwen Qu
+//## * Da Yan
+//########################################################################
+
+#include "app/gaston/GastonWorker.h"
+
+#include "system/timetrack.h"
+char infile[300];
+extern TimeTracker tt1,tt2,tt3,tt4,tt5;
+
+void parse_args(int argc, char **argv) {
+		extern char *optarg;
+		int c;
+
+		if (argc < 2)
+			cout << "usage:  -i <infile> -s <support> -n <thread_number>\n";
+		else {
+			while ((c = getopt(argc, argv, "e:i:s:n:c:T:t:b:B")) != -1) {
+				switch (c) {
+				case 'i': //input files
+					sprintf(infile, "%s", optarg);
+					break;
+				case 's': //support value for L2
+					minsup = atof(optarg);
+					break;
+				case 'n':
+					THREADS = atoi(optarg);
+					break;
+				case 'B': // the input file is ASCII(or binary file)
+					binary_input = true;
+					break;
+				case 'c': //how many elements per access in OMP parallel-for that processes PDB rows
+					CHUNK = atoi(optarg);
+					break;
+				case 'T': // projDB size threshold, below which task is run in a single thread
+					tauDB_omp = atof(optarg);
+					break;
+				case 't': // projDB size threshold, below which task is run in a single thread
+					tauDB_singlethread = atof(optarg);
+					break;
+				case 'e': // task running time threshold, below which task is inserted into queue directly
+					tauDB_time = atof(optarg);
+					break;
+				case 'b': //how many tasks per access to the task queue
+					batch_size = atoi(optarg);
+					break;
+				}
+			}
+		}
+	}
+
+
+int main(int argc, char **argv){
+	parse_args(argc, argv);
+	TimeTracker tt;
+	
+    GastonWorker worker(infile);
+
+	worker.read(worker.file_name);
+	tt.start();
+	worker.run();
+	cout << tt.stop() << endl;
+	cout << "tt1: " << tt1.total_time << endl;
+	
+	cout << "tt2: "<< tt2.total_time << endl;
+	return 0;
+}
+
+
+//./run -s 100 -i prefixspan/test.data -n 4
